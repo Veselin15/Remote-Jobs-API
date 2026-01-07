c90993a33c6cafa8ed4a7fc78bbecd3b7fc3f3c2
veski4a <veskoveskov2006@gmail.com>
2026-01-07 10:27:15 +0200

    making the api work only for linkedin

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -1,20 +1,13 @@
 from celery import shared_task
 import subprocess
 
-
 @shared_task
 def run_scrapers(keyword='Python', location='Europe'):
-    # 1. Run LinkedIn Spider with Dynamic Arguments
-    # We pass -a keyword=... -a location=... to Scrapy
+    # Run ONLY the LinkedIn Spider
     subprocess.run([
         "scrapy", "crawl", "linkedin",
         "-a", f"keyword={keyword}",
         "-a", f"location={location}"
     ], cwd="/app/scraper_service")
 
-    # 2. Run Python.org spider ONLY if the keyword is related to Python
-    # (Since python.org doesn't list C++ jobs)
-    if 'python' in keyword.lower():
-        subprocess.run(["scrapy", "crawl", "remote_python"], cwd="/app/scraper_service")
-
-    return f"Scraping Finished for {keyword} in {location}"
+    return f"LinkedIn Scraping Finished for {keyword} in {location}"
